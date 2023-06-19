0c3cd2dd56b8feb6c012b02cb00465fc135f1388
Danilo Horta <danilo.horta@pm.me>
2023-06-19 15:59:38 +0100

    First commit on deciphon-sna

---
+++ deciphon-snap/tests/test_read.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from deciphon_snap.read_snap import read_snap
+
+import pytest
+
+
+@pytest.fixture()
+def files_path() -> Path:
+    current_dir = os.path.dirname(__file__)
+    return Path(current_dir) / "files"
+
+
+def test_read_snap(files_path: Path):
+    snap_file = read_snap(files_path / "example.dcs")
+    assert len(snap_file.products) == 470
+    prod = snap_file.products[0]
+    assert len(prod.match_list) == 462
+    assert str(prod.match_list[0]) == "(∅,S,∅,∅)"
