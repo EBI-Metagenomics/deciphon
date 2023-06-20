362571862a0bb1d52f4e2ff7cfc6b40e1cb352e4
Danilo Horta <danilo.horta@pm.me>
2023-06-20 10:25:02 +0100

    First commit on deciphon-py

---
+++ deciphon-py/tests/test_press.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+from pathlib import Path
+
+from typer.testing import CliRunner
+
+from deciphon.cli import app
+
+runner = CliRunner()
+
+
+def test_press(tmp_path: Path, files_path: Path):
+    shutil.copy(files_path / "minifam.hmm", tmp_path)
+    os.chdir(tmp_path)
+
+    result = runner.invoke(app, ["press", "minifam.hmm", "11"])
+    assert result.exit_code == 0
+
+    assert Path("minifam.dcp").stat().st_size == 9933912
