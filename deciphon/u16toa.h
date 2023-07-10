db8d5265f505273666c42fe41799228f266ce646
Danilo Horta <danilo.horta@pm.me>
2023-07-10 17:05:01 +0100

    u16toa header only

--- deciphon/u16toa.h
+++ deciphon/u16toa.h
@@ -3,6 +3,26 @@
 
 #include <stdint.h>
 
-unsigned u16toa(char *str, uint16_t num);
+/* Convert uint16_t to null-terminated string.
+ * Return the string length. */
+static unsigned u16toa(char *str, uint16_t num)
+{
+  char const *const begin = str;
+  *str = '0';
+
+  unsigned denom = 10000;
+  while (denom > 1 && !(num / denom))
+    denom /= 10;
+
+  while (denom)
+  {
+    *str = (char)('0' + (num / denom));
+    num %= denom;
+    ++str;
+    denom /= 10;
+  }
+  *str = 0;
+  return (unsigned)(str - begin);
+}
 
 #endif
