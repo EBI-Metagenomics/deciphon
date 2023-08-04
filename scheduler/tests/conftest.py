223f918240873898d3b5ff33b2dae2ff56fee529
Danilo Horta <danilo.horta@pm.me>
2023-08-04 22:29:26 +0100

    health check for mosquitto testcontainers

--- scheduler/tests/conftest.py
+++ scheduler/tests/conftest.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 import pytest
 from testcontainers.core.container import DockerContainer
 from testcontainers.minio import MinioContainer
+import paho.mqtt.client
 
 from deciphon_scheduler.settings import Settings
+from testcontainers.core.waiting_utils import wait_container_is_ready
 
 
 @pytest.fixture(scope="package")
@@ -35,6 +37,16 @@ class MosquittoContainer(DockerContainer):
     def __init__(self, image="eclipse-mosquitto:2.0.15", port=1883):
         super(MosquittoContainer, self).__init__(image)
         self.port = port
-
         self.with_exposed_ports(self.port)
+
+    @wait_container_is_ready(ConnectionError)
+    def _healthcheck(self):
+        host = self.get_container_host_ip()
+        port = int(self.get_exposed_port(self.port))
+        paho.mqtt.client.Client().connect(host, port, 5)
+
+    def start(self):
         self.with_command("mosquitto -c /mosquitto-no-auth.conf")
+        super().start()
+        self._healthcheck()
+        return self
