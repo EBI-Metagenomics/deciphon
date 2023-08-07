e338f220ded6fcfc841cdd4f254851989d11ee13
Danilo Horta <danilo.horta@pm.me>
2023-08-07 12:14:24 +0100

    simplify hmm tests

--- scheduler/tests/scheduler/test_hmms.py
+++ scheduler/tests/scheduler/test_hmms.py
@@ -1,40 +1,48 @@
 from fastapi.testclient import TestClient
 
-MINIFAM_HMM = {"name": "minifam.hmm"}
+
+def test_hmm_not_found(compose):
+    with TestClient(compose["app"]) as client:
+        assert client.get("/hmms/1").status_code == 404
 
 
-def test_hmm_not_found(app):
-    with TestClient(app) as client:
-        response = client.get("/hmms/1")
-        assert response.status_code == 404
+def test_presigned_hmm_url(compose):
+    with TestClient(compose["app"]) as client:
+        assert client.get("/hmms/presigned-upload/minifam.hmm").status_code == 200
 
 
-def test_create_hmm(app):
-    with TestClient(app) as client:
-        response = client.get("/hmms/presigned-upload")
-        response = client.post("/hmms/", json=MINIFAM_HMM)
-        assert response.status_code == 201
+def test_create_hmm_failure(compose):
+    with TestClient(compose["app"]) as client:
+        client.get("/hmms/presigned-upload/minifam.hmm")
+        assert client.post("/hmms/", json={"name": "minifam.hmm"}).status_code == 422
 
 
-def test_read_hmm(app):
-    with TestClient(app) as client:
-        response = client.post("/hmms/", json=MINIFAM_HMM)
-        assert response.status_code == 201
-        response = client.get("/hmms/1")
-        assert response.status_code == 200
+def test_create_hmm(compose, files, s3_upload):
+    with TestClient(compose["app"]) as client:
+        response = client.get("/hmms/presigned-upload/minifam.hmm")
+        s3_upload(response.json(), files / "minifam.hmm")
+        assert client.post("/hmms/", json={"name": "minifam.hmm"}).status_code == 201
 
 
-def test_read_hmms(app):
-    with TestClient(app) as client:
-        response = client.post("/hmms/", json=MINIFAM_HMM)
-        assert response.status_code == 201
-        response = client.get("/hmms")
-        assert response.status_code == 200
+def test_read_hmm(compose, files, s3_upload):
+    with TestClient(compose["app"]) as client:
+        response = client.get("/hmms/presigned-upload/minifam.hmm")
+        s3_upload(response.json(), files / "minifam.hmm")
+        client.post("/hmms/", json={"name": "minifam.hmm"})
+        assert client.get("/hmms/1").status_code == 200
 
 
-def test_delete_hmm(app):
-    with TestClient(app) as client:
-        response = client.post("/hmms/", json=MINIFAM_HMM)
-        assert response.status_code == 201
-        response = client.delete("/hmms/1")
-        assert response.status_code == 204
+def test_read_hmms(compose, files, s3_upload):
+    with TestClient(compose["app"]) as client:
+        response = client.get("/hmms/presigned-upload/minifam.hmm")
+        s3_upload(response.json(), files / "minifam.hmm")
+        client.post("/hmms/", json={"name": "minifam.hmm"})
+        assert client.get("/hmms").status_code == 200
+
+
+def test_delete_hmm(compose, files, s3_upload):
+    with TestClient(compose["app"]) as client:
+        response = client.get("/hmms/presigned-upload/minifam.hmm")
+        s3_upload(response.json(), files / "minifam.hmm")
+        client.post("/hmms/", json={"name": "minifam.hmm"})
+        assert client.delete("/hmms/1").status_code == 204
