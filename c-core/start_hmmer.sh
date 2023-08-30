76f5a67bb84a823dd52ad152e7c43909e6699ed6
Danilo Horta <danilo.horta@pm.me>
2023-08-30 16:56:25 +0100

    Refactor start_hmmer.sh script to include cleanup function and trap for error handling

--- c-core/start_hmmer.sh
+++ c-core/start_hmmer.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -x
 
+cleanup() {
+  rv=$?
+  if [ $rv -ne 0 ]
+  then
+    cat h3daemon.stdout h3daemon.stderr
+  fi
+  rm -rf h3daemon.stdout h3daemon.stderr || true
+  exit $rv
+}
+trap "cleanup" EXIT
+
 pipx run h3daemon start "$1" --port 51371 --force \
     --stdout h3daemon.stdout \
     --stderr h3daemon.stderr || exit 1
 
-if ! pipx run h3daemon isready "$1" --wait
-then
-    cat h3daemon.stdout h3daemon.stderr
-    rm -f h3daemon.stdout h3daemon.stderr || true
-    exit 1
-fi
+pipx run h3daemon isready "$1" --wait
