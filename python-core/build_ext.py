a68f48f2b383e7c1c8ec57ab6316587951cce815
Danilo Horta <danilo.horta@pm.me>
2023-09-08 12:26:37 +0100

    release for testing

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -69,7 +69,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.9.3",
+        "c-core-v0.9.4",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
