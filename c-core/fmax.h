dd9af5cbff35464e769c5f3c35aad7d471c0118d
Danilo Horta <danilo.horta@pm.me>
2023-09-11 12:57:17 +0100

    use fmax for aarch64

--- c-core/fmax.h
+++ c-core/fmax.h
@@ -2,11 +2,13 @@
 #define DECIPHON_FMAX_H
 
 #include "compiler.h"
+#include <math.h>
 
 DCP_CONST float dcp_fmax(float a, float b)
 {
 #if __aarch64__
-  __asm__("fmaxnm %s0, %s1, %s2" : "=w"(a) : "w"(a), "w"(b));
+  a = fmax(a, b);
+  // __asm__("fmaxnm %s0, %s1, %s2" : "=w"(a) : "w"(a), "w"(b));
 #else
   a = a > b ? a : b;
 #endif
