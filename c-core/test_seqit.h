1d33520d33a68076ad28236c4bfd36212a4c3d17
Danilo Horta <danilo.horta@pm.me>
2023-09-14 15:24:26 +0100

    seqit

---
+++ c-core/test_seqit.h
@@ -0,0 +1,40 @@
+#ifndef DECIPHON_TEST_SEQIT_H
+#define DECIPHON_TEST_SEQIT_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct seq
+{
+  int id;
+  char name[256];
+  char const *data;
+};
+
+struct seqit
+{
+  struct seq seq;
+  int size;
+  char const *(*callb)(void);
+};
+
+static struct seqit seqit_init(int size, char const *(*callb)(void))
+{
+  return (struct seqit){{-1, {0}, NULL}, size, callb};
+}
+
+static struct seq const *seqit_next(struct seqit *x)
+{
+  if (x->size > 0)
+  {
+    x->size -= 1;
+    x->seq.id += 1;
+    sprintf(x->seq.name, "name%d", x->seq.id);
+    x->seq.data = (*x->callb)();
+    return &x->seq;
+  }
+  return NULL;
+}
+
+#endif
