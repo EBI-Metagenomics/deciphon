32c98da4f313b6d3234d628f68ec65e57d818d8a
Danilo Horta <danilo.horta@pm.me>
2023-09-19 16:31:10 +0100

    fix fmax

--- c-core/fmax.h
+++ c-core/fmax.h
@@ -7,12 +7,11 @@
 DCP_CONST float dcp_fmax(float a, float b)
 {
 #if __aarch64__
-  a = fmax(a, b);
+  return fmax(a, b);
   // __asm__("fmaxnm %s0, %s1, %s2" : "=w"(a) : "w"(a), "w"(b));
 #else
-  a = a > b ? a : b;
+  return a > b ? a : b;
 #endif
-  return a;
 }
 
 #endif
