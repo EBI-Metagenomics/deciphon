eb77cc25bfff804ad48a37c52d38e64b2b00e7f1
Danilo Horta <danilo.horta@pm.me>
2023-09-19 11:37:16 +0100

    read module

---
+++ c-core/read.h
@@ -0,0 +1,41 @@
+#ifndef DECIPHON_READ_H
+#define DECIPHON_READ_H
+
+#include "array_size.h"
+#include "imm/imm.h"
+#include "lip/lip.h"
+#include "rc.h"
+#include <string.h>
+
+static inline int read_mapsize(struct lip_file *file, unsigned size)
+{
+  unsigned sz = 0;
+  if (!lip_read_map_size(file, &sz)) return DCP_EFREAD;
+  return size == sz ? 0 : DCP_EFDATA;
+}
+
+static inline int read_key(struct lip_file *file, char const key[])
+{
+  unsigned size = 0;
+  char buf[32] = {0};
+
+  if (!lip_read_str_size(file, &size)) return DCP_EFREAD;
+
+  if (size > array_size(buf)) return DCP_EFDATA;
+
+  if (!lip_read_str_data(file, size, buf)) return DCP_EFREAD;
+
+  if (size != (unsigned)strlen(key)) return DCP_EFDATA;
+
+  return strncmp(key, buf, size) ? DCP_EFDATA : 0;
+}
+
+#define read_int(stream, ptr) (lip_read_int(stream, ptr) ? 0 : DCP_EFREAD)
+#define read_float(stream, ptr) (lip_read_float(stream, ptr) ? 0 : DCP_EFREAD)
+
+static inline int read_abc(struct lip_file *stream, struct imm_abc *abc)
+{
+  return imm_abc_unpack(abc, stream) ? DCP_EFREAD : 0;
+}
+
+#endif
