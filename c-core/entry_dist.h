06f3cd08f2043dca961eff8868fef4c4ab56b6df
Danilo Horta <danilo.horta@pm.me>
2023-09-19 11:37:12 +0100

    validate entry_dist

--- c-core/entry_dist.h
+++ c-core/entry_dist.h
@@ -1,11 +1,18 @@
 #ifndef DECIPHON_ENTRY_DIST_H
 #define DECIPHON_ENTRY_DIST_H
 
+#include <stdbool.h>
+
 enum dcp_entry_dist
 {
   DCP_ENTRY_DIST_NULL,
   DCP_ENTRY_DIST_UNIFORM,
   DCP_ENTRY_DIST_OCCUPANCY,
 };
+
+static inline bool dcp_entry_dist_valid(int x)
+{
+  return x > DCP_ENTRY_DIST_NULL && x <= DCP_ENTRY_DIST_OCCUPANCY;
+}
 
 #endif
