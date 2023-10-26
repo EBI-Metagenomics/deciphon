34d6954f2e03b7d6fa28411f3d50532ba5ad7085
Danilo Horta <danilo.horta@pm.me>
2023-10-26 17:21:03 +0100

    Update EBI-Metagenomics and deciphon versions to latest

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -56,7 +56,7 @@ EXTS = [
         "./hmmer-reader",
         CMAKE_OPTS,
     ),
-    Ext("EBI-Metagenomics", "imm", "v5.3.5", "./", CMAKE_OPTS + CPM_OPTS),
+    Ext("EBI-Metagenomics", "imm", "v5.3.6", "./", CMAKE_OPTS + CPM_OPTS),
     Ext("nanomsg", "nng", "v1.5.2", "./", CMAKE_OPTS + NNG_OPTS),
     Ext(
         "EBI-Metagenomics",
@@ -68,7 +68,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.12.3",
+        "c-core-v0.12.4",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
