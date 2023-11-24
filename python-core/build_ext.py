94c21ff379db4d7d08aec033e960960edde9e95f
Danilo Horta <danilo.horta@pm.me>
2023-11-24 14:29:11 +0000

    Update c-core version to v0.13.11

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -68,7 +68,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.13.10",
+        "c-core-v0.13.11",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
