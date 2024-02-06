6469018c1a8609bf7fed024d7ac948c3c44838c4
Danilo Horta <danilo.horta@pm.me>
2024-02-06 14:28:40 +0000

    cleanup attributes

--- c-core/compiler_attributes.h
+++ c-core/compiler_attributes.h
@@ -4,7 +4,7 @@
 #define __noreturn __attribute__((__noreturn__))
 #define __always_inline inline __attribute__((__always_inline__))
 
-#define __attribute_const__ __attribute__((__const__))
-#define __attribute_pure__ __attribute__((__pure__))
+#define __attribute_const __attribute__((__const__))
+#define __attribute_pure __attribute__((__pure__))
 
 #endif
