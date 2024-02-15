4515d77381f83c7f06e7c1a17558077b41f028c0
Danilo Horta <danilo.horta@pm.me>
2024-02-15 15:39:16 +0000

    Update EBI-Metagenomics versions and core to 0.21.7

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -56,7 +56,7 @@ EXTS = [
         "./hmmer-reader",
         CMAKE_OPTS,
     ),
-    Ext("EBI-Metagenomics", "imm", "v5.4.4", "./", CMAKE_OPTS + CPM_OPTS),
+    Ext("EBI-Metagenomics", "imm", "v5.5.0", "./", CMAKE_OPTS + CPM_OPTS),
     Ext("nanomsg", "nng", "v1.5.2", "./", CMAKE_OPTS + NNG_OPTS),
     Ext(
         "EBI-Metagenomics",
@@ -68,7 +68,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.17.1",
+        "c-core-v0.17.2",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
