437ef669e01ac008a262b7dc77521a184138574d
Danilo Horta <danilo.horta@pm.me>
2024-02-15 17:19:59 +0000

    Update Deciphon core to v0.17.3 and v0.21.9

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -68,7 +68,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.17.2",
+        "c-core-v0.17.3",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
