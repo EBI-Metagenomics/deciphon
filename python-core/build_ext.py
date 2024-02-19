5d8102840613a70c548fbbbd500eb4f79f02fee5
Danilo Horta <danilo.horta@pm.me>
2024-02-19 11:22:01 +0000

    Update c-core to v0.18.2 and bump version to 0.21.12

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -68,7 +68,7 @@ EXTS = [
     Ext(
         "EBI-Metagenomics",
         "deciphon",
-        "c-core-v0.18.0",
+        "c-core-v0.18.2",
         "./c-core",
         CMAKE_OPTS + CPM_OPTS,
     ),
