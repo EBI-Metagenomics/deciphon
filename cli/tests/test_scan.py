217432c57bf9c7f420662e1d6618e41189d2da7d
Danilo Horta <danilo.horta@pm.me>
2024-02-19 13:39:44 +0000

    Update version and checksum in pyproject

--- cli/tests/test_scan.py
+++ cli/tests/test_scan.py
@@ -36,7 +36,7 @@ def test_scan(tmp_path: Path, files_path: Path):
     assert snap.exists()
     shutil.unpack_archive(snap, format="zip")
     products = basedir(snap) / "products.tsv"
-    assert checksum(products)[:8] == "1609c14c"
+    assert checksum(products)[:8] == "3d5bf4b8"
 
 
 def checksum(filename: Path):
