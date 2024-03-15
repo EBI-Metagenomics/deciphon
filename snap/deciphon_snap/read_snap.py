518eef07df3b41f774e41220e61a40582a3f183c
Danilo Horta <danilo.horta@pm.me>
2024-03-15 15:49:57 +0000

    Read the zip file entirely before constructing filesystem

--- snap/deciphon_snap/read_snap.py
+++ snap/deciphon_snap/read_snap.py
@@ -1,4 +1,4 @@
-from fsspec.implementations.cached import SimpleCacheFileSystem
+from fsspec.implementations.memory import MemoryFile
 from fsspec.implementations.zip import ZipFileSystem
 
 from deciphon_snap.path_like import PathLike
@@ -9,4 +9,6 @@ __all__ = ["read_snap"]
 
 def read_snap(filename: PathLike):
     fo = filename.decode() if isinstance(filename, bytes) else str(filename)
-    return SnapFile(SimpleCacheFileSystem(fs=ZipFileSystem(fo)))
+    with open(fo, "rb") as f:
+        fs = ZipFileSystem(fo=MemoryFile(data=f.read()))
+        return SnapFile(fs)
