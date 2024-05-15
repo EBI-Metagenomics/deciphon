819fbc555980583504d12b59a8ec78c0eb1d6caf
Danilo Horta <danilo.horta@pm.me>
2024-05-15 02:01:10 +0100

    delete eval folder

--- eval/score-quantitatively.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-root=$1
-pfam_file=$root/Pfam-A.hmm
-genome_dirs=$(find $root -maxdepth 4 -type f -name "cds_from_genomic.dcs" -exec dirname {} \; | tr '\n' ' ')
-
-cmd="python script/score_quantitatively.py $root/quantitatively.parquet $pfam_file $genome_dirs"
-bsub -g /horta/eval "$cmd"
