d9d0deca23a051ccd1dbac799882c3ae40bef5da
Danilo Horta <danilo.horta@pm.me>
2024-09-26 12:49:37 +0100

    Update build_ext.py for improved build process
    - Refactor build_and_install function for better modularity
    - Add force parameter to control re-cloning of repositories
    - Update main script to use new build_and_install signature
    - Add gitpython dependency in pyproject.toml

--- python-core/build_ext.py
+++ python-core/build_ext.py
@@ -6,56 +6,49 @@ from subprocess import check_call
 from cffi import FFI
 from git import Repo
 
-CWD = Path(".").resolve()
-TMP = CWD / ".build_ext"
-PKG = CWD / "deciphon_core"
 
-
-def make(
-    cwd: Path,
-    args: list[str] = [],
-):
+def make(cwd: Path, args: list[str] = []):
     check_call(["make"] + args, cwd=cwd)
 
 
-def build_and_install(root: Path, prefix: str, prj_dir: str, git_url: str):
-    git_dir = git_url.split("/")[-1].split(".")[-1][:-4]
-    if not (root / git_dir).exists():
-        Repo.clone_from(git_url, root / git_dir, depth=1)
-    bld_dir = root / prj_dir
+def build_and_install(
+    root: Path, prefix: str, git_url: str, prj_dir: str, dst_dir: str, force: bool
+):
+    os.makedirs(root / ".gitdir", exist_ok=True)
+    if force or not (root / dst_dir).exists():
+        shutil.rmtree(root / ".gitdir" / dst_dir, ignore_errors=True)
+        Repo.clone_from(git_url, root / ".gitdir" / dst_dir, depth=1)
+        shutil.rmtree(root / dst_dir, ignore_errors=True)
+        shutil.move(root / ".gitdir" / dst_dir / prj_dir, root / dst_dir)
+
     args = [
         f"C_INCLUDE_PATH={prefix}/include",
         f"LIBRARY_PATH={prefix}/lib",
         "CFLAGS=-std=c11 -O3 -fPIC",
     ]
-    make(bld_dir, args)
-    make(bld_dir, ["install", f"PREFIX={prefix}"])
+    make(root / dst_dir, args)
+    make(root / dst_dir, ["install", f"PREFIX={prefix}"])
 
 
 if __name__ == "__main__":
-    shutil.rmtree(TMP, ignore_errors=True)
-    os.makedirs(TMP, exist_ok=True)
+    CWD = Path(".").resolve()
+    TMP = CWD / ".build_ext"
+    PKG = CWD / "deciphon_core"
 
     url = "https://github.com/EBI-Metagenomics/lite-pack.git"
-    build_and_install(TMP / "lite-pack", str(PKG), ".", url)
-
-    url = "https://github.com/EBI-Metagenomics/lite-pack.git"
-    build_and_install(TMP / "lite-pack", str(PKG), "ext/", url)
+    build_and_install(TMP, str(PKG), url, ".", "lite-pack", True)
+    build_and_install(TMP, str(PKG), url, "ext/", "lite-pack-ext", True)
 
     url = "https://github.com/EBI-Metagenomics/imm.git"
-    build_and_install(TMP / "imm", str(PKG), ".", url)
+    build_and_install(TMP, str(PKG), url, ".", "imm", True)
 
     url = "https://github.com/EBI-Metagenomics/hmmer3.git"
-    build_and_install(TMP / "hmmer3", str(PKG), "hmmer-reader/", url)
-
-    url = "https://github.com/EBI-Metagenomics/hmmer3.git"
-    build_and_install(TMP / "hmmer3", str(PKG), "h3result/", url)
-
-    url = "https://github.com/EBI-Metagenomics/hmmer3.git"
-    build_and_install(TMP / "hmmer3", str(PKG), "h3client/", url)
+    build_and_install(TMP, str(PKG), url, "hmmer-reader/", "hmmer-reader", True)
+    build_and_install(TMP, str(PKG), url, "h3result/", "h3result", True)
+    build_and_install(TMP, str(PKG), url, "h3client/", "h3client", True)
 
     url = "https://github.com/EBI-Metagenomics/deciphon.git"
-    build_and_install(TMP / "deciphon", str(PKG), "c-core/", url)
+    build_and_install(TMP, str(PKG), url, "c-core/", "c-core", True)
 
     ffibuilder = FFI()
 
@@ -74,11 +67,11 @@ if __name__ == "__main__":
             "imm",
             "lio",
             "lite_pack",
+            "gomp" if os.uname().sysname == "Linux" else "omp",
         ],
         library_dirs=[str(PKG / "lib")],
         include_dirs=[str(PKG / "include")],
-        extra_compile_args=["-fopenmp"] if os.uname().sysname == "Linux" else [],
-        extra_link_args=["-lgomp"] if os.uname().sysname == "Linux" else [],
+        extra_compile_args=["-fopenmp"],
     )
 
     ffibuilder.compile(verbose=True)
