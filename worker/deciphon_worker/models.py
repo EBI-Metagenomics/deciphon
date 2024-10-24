9bfe91c33b77730edbc5c1f4b13f7b912dac8540
Danilo Horta <danilo.horta@pm.me>
2024-10-24 22:52:34 +0100

    Enhance CLI logging and refactor log levels 🎉
    - Updated `cli.py` to include a logging option using `typer.Option`.
    - Introduced `setup_logger` in `logging.py` for configuring log levels.
    - Removed redundant `LogLevel` class from `models.py`.
    - Bumped version from 0.1.0 to 0.1.1 in `pyproject.toml`.
    
    These improvements bring better log management to CLI commands, enabling
    customizable log levels. Refactoring the `LogLevel` class promotes code
    reusability and maintainability. ⚙️

--- worker/deciphon_worker/models.py
+++ worker/deciphon_worker/models.py
@@ -116,16 +116,3 @@ class Scan(BaseModel):
     multi_hits: bool
     hmmer3_compat: bool
     seqs: list[Seq]
-
-
-class LogLevel(str, Enum):
-    debug = "debug"
-    info = "info"
-    warning = "warning"
-    error = "error"
-    critical = "critical"
-
-    # Enum of Python3.10 returns a different string representation.
-    # Make it return the same as in Python3.11
-    def __str__(self):
-        return str(self.value)
