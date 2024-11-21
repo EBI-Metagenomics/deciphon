85ea6e1aba1fef0cc328c2d74283c2855f425635
Danilo Horta <danilo.horta@pm.me>
2024-11-21 15:58:50 +0000

    Enhance pipx install for h3daemon 🎉
    - Adjusted start_daemon.sh to install h3daemon[cli]

--- c-core/start_daemon.sh
+++ c-core/start_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-command -v h3daemon >/dev/null || pipx install h3daemon
+command -v h3daemon >/dev/null || pipx install h3daemon[cli]
 
 h3daemon start $1 --port $2 >/dev/null
 h3daemon ready $1 --wait >/dev/null
