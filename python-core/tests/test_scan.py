f3de1342ef803498764092fe723b42e833a47629
Danilo Horta <danilo.horta@pm.me>
2024-12-01 00:36:09 +0000

    Refactor test_scan.py import and function signatures 🛠️
    - Updated import statement to use ensure_pressed instead of H3File.
    - Adjusted function signature in run_scan to use HMMFile consistently.
    - Resolved Path for hmm variable in test_scan function for clarity.
    - Ensured proper handling of pressed files with ensure_pressed function.
    
    These changes ensure consistency in handling file operations, leveraging
    the ensure_pressed function for robust file management. The refactor
    also enhances code readability and maintainability by standardizing the
    usage of the HMMFile class and improving import accuracy.

--- python-core/tests/test_scan.py
+++ python-core/tests/test_scan.py
@@ -6,7 +6,7 @@ import shutil
 from pathlib import Path
 
 from deciphon_schema import DBFile, Gencode, HMMFile, NewSnapFile
-from h3daemon.hmmfile import HMMFile as H3File
+from h3daemon.ensure_pressed import ensure_pressed
 from h3daemon.polling import wait_until
 from h3daemon.sched import SchedContext
 
@@ -24,14 +24,14 @@ def checksum(filename: Path):
     return hash_obj.hexdigest()
 
 
-def run_scan(hmm: Path, hmmfile: H3File, num_threads: int, cache: bool):
+def run_scan(hmm: Path, hmmfile: HMMFile, num_threads: int, cache: bool):
     shutil.rmtree("snap.dcs", ignore_errors=True)
     shutil.rmtree("snap", ignore_errors=True)
     snapfile = NewSnapFile(path=Path("snap.dcs").absolute())
 
     with SchedContext(hmmfile) as sched:
         wait_until(sched.is_ready)
-        dbfile = DBFile(path=HMMFile(path=hmm).dbpath.path)
+        dbfile = DBFile(path=hmmfile.dbpath.path)
         batch = Batch()
         for seq in sequences:
             batch.add(seq)
@@ -102,13 +102,13 @@ def test_scan(tmp_path, files_path: Path):
     shutil.copy(files_path / "minifam.hmm", tmp_path)
     os.chdir(tmp_path)
 
-    hmm = Path("minifam.hmm")
+    hmm = Path("minifam.hmm").resolve()
     with PressContext(HMMFile(path=hmm), Gencode(1)) as press:
         while not press.end():
             press.next()
 
-    hmmfile = H3File(hmm)
-    hmmfile.ensure_pressed()
+    hmmfile = HMMFile(path=hmm)
+    ensure_pressed(hmmfile)
 
     run_scan(hmm, hmmfile, 1, False)
     run_scan(hmm, hmmfile, 1, True)
