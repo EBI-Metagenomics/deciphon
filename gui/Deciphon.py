56057cae7a1993b1684f671b79703c898063d610
Danilo Horta <danilo.horta@pm.me>
2024-12-09 15:12:16 +0000

    Enhance build process for cross-platform support 🚀
    - Updated `.github/workflows/build-gui.yml` to support MacOS and Linux.
    - Enhanced build steps: added OS-specific Nuitka strategies.
    - Removed outdated compilation options in `gui/Deciphon.py`.

--- gui/Deciphon.py
+++ gui/Deciphon.py
@@ -1,6 +1,5 @@
 # Compilation mode, support OS-specific options
 # nuitka-project-if: {OS} in ("Darwin"):
-#    nuitka-project: --mode=app
 #    nuitka-project: --macos-target-arch=arm64
 #    nuitka-project: --macos-app-icon={MAIN_DIRECTORY}/logo.icns
 #    nuitka-project: --macos-app-name=Deciphon
@@ -11,7 +10,6 @@
 #    nuitka-project: --include-package-data=hmmer
 #    nuitka-project: --include-package-data=deciphon_core
 # nuitka-project-if: {OS} in ("Linux"):
-#    nuitka-project: --mode=onefile
 #    nuitka-project: --linux-icon={MAIN_DIRECTORY}/logo.png
 # nuitka-project: --product-name=Deciphon
 # nuitka-project: --product-version=1.0.0
