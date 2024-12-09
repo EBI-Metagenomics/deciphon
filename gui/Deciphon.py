2b30ab1dc18c10ec9a57657ba0e174e2943a6f48
Danilo Horta <danilo.horta@pm.me>
2024-12-09 13:53:34 +0000

    Update GUI build paths and icon references 🛠️
    - Modified `.github/workflows/build-gui.yml` to update script and output paths.
    - Updated `gui/Deciphon.py` to use relative icon path references for macOS
    and Linux builds.

--- gui/Deciphon.py
+++ gui/Deciphon.py
@@ -2,7 +2,7 @@
 # nuitka-project-if: {OS} in ("Darwin"):
 #    nuitka-project: --macos-create-app-bundle
 #    nuitka-project: --macos-target-arch=arm64
-#    nuitka-project: --macos-app-icon=logo.icns
+#    nuitka-project: --macos-app-icon={MAIN_DIRECTORY}/logo.icns
 #    nuitka-project: --macos-app-name=Deciphon
 #    nuitka-project: --macos-app-mode=gui
 #    nuitka-project: --macos-app-version=1.0.0
@@ -14,7 +14,7 @@
 #    nuitka-project: --include-package-data=deciphon_core
 # nuitka-project-if: {OS} in ("Linux"):
 #    nuitka-project: --standalone
-#    nuitka-project: --linux-icon=logo.png
+#    nuitka-project: --linux-icon={MAIN_DIRECTORY}/logo.png
 # nuitka-project: --product-name=Deciphon
 # nuitka-project: --product-version=1.0.0
 # nuitka-project: --output-dir=build
